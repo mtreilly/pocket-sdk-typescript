da8057cbed12a4618501bcb320072418d4333366
Micheál Reilly-Sendal <micheal@local.dev>
2023-04-10 22:51:01 +0200

    Added test for create Auth Url

--- auth/createAuthUrl.ts
+++ auth/createAuthUrl.ts
@@ -1,4 +1,4 @@
-type PlatformType = "ios" | "other";
+export type PlatformType = "ios" | "other";
 
 export default function createAuthUrl(requestToken: string, redirectUri: string, platform: PlatformType): string {
   if (platform === "ios") {
