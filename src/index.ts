798779b0b66c9a31718df792c24965e5aded9848
Micheál Reilly-Sendal <micheal@local.dev>
2023-04-23 20:51:08 +0200

    Updated build, removed some unessary declartions

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,3 @@ export { default as PocketSDK } from "./sdk";
 export { default as getRequestToken } from "./auth/getRequestToken";
 export { default as getAccessToken } from "./auth/getAccessToken";
 export { default as createAuthUrl } from "./auth/createAuthUrl";
-
-export function add(x: number, y: number): number {
-  return x + y;
-}
