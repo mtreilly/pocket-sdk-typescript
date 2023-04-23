ad14903da9263840a5fc0dda3b854cd54d60c124
Micheál Reilly-Sendal <micheal@local.dev>
2023-04-23 19:41:14 +0200

    Added additional auth exports and support for commonjs

--- dist/lib/index.js
+++ dist/lib/index.js
@@ -1,3 +1,5 @@
 export { default as PocketSDK } from "./sdk";
 export { default as getRequestToken } from "./auth/getRequestToken";
+export { default as getAccessToken } from "./auth/getAccessToken";
+export { default as createAuthUrl } from "./auth/createAuthUrl";
 //# sourceMappingURL=index.js.map
