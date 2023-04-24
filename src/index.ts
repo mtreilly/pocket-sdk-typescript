bee4d9f295d9434c714f4fb6c7514eafc68d9c08
Micheál Reilly-Sendal <micheal@local.dev>
2023-04-24 17:14:22 +0200

    Added export of interfaces

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,7 @@ export { default as PocketSDK } from "./sdk";
 export { default as getRequestToken } from "./auth/getRequestToken";
 export { default as getAccessToken } from "./auth/getAccessToken";
 export { default as createAuthUrl } from "./auth/createAuthUrl";
+
+// Export all interfaces
+export * from "./interfaces/responses";
+export * from "./interfaces/requests";
